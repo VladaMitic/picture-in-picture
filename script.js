b7311a2fcb623a683df6a20e335725cd3e64be56
unknown <rokagresor@gmail.com>
2020-09-12 00:55:51 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,27 @@
+const videoElement = document.getElementById('video');
+const button = document.getElementById('button');
+
+//prompt user to select video, pass the video to video element, and play
+
+async function selectMediaStream() {
+    try {
+        const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+        videoElement.srcObject = mediaStream;
+        videoElement.onloadedmetadata = () => {
+            videoElement.play();
+        }
+    } catch (error) {
+
+    }
+}
+
+button.addEventListener('click', async () => {
+    //Disable button
+    button.disabled = true;
+    //Start picture in picture
+    await videoElement.requestPictureInPicture();
+    //reset button
+    button.disabled = false;
+});
+
+selectMediaStream();
